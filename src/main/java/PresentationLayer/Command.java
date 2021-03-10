3429a7e1f8cd28bc52300a6aae995355d316c2c7
fdinsen <cph-fd77@cphbusiness.dk>
2021-03-10 12:13:45 +0100

    Adds command pattern

---
+++ src/main/java/PresentationLayer/Command.java
@@ -0,0 +1,27 @@
+package PresentationLayer;
+
+import Exceptions.LoginSampleException;
+import java.util.HashMap;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+abstract class Command {
+
+    private static HashMap<String, Command> commands;
+
+    private static void initCommands() {
+        commands = new HashMap<>();
+    }
+
+    static Command from( HttpServletRequest request ) {
+        String targetName = request.getParameter( "target" );
+        if ( commands == null ) {
+            initCommands();
+        }
+        return commands.getOrDefault(targetName, new UnknownCommand()); // unknowncommand er default.
+    }
+
+    abstract String execute( HttpServletRequest request, HttpServletResponse response ) 
+            throws LoginSampleException;
+
+}
